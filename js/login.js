3d375e5972298748e27eec74f6a9fdce6428a566
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-18 19:12:53 +0600

    [DONE] I Bank

--- js/login.js
+++ js/login.js
@@ -6,7 +6,27 @@ document.getElementById('login-submit').addEventListener('click', function () {
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
+    // Check for valid email and password
     if (userEmail === 'jibon@ibank.com' && userPassword === 'jibon') {
-        window.location.href = 'banking.html';
+        // Show success message and redirect
+        document.getElementById('login-error').innerText = '';
+        showSuccessMessage('Login successful! Redirecting to the banking page...');
+        setTimeout(function () {
+            window.location.href = 'banking.html';
+        }, 2000);
+    } else {
+        // Show error message
+        document.getElementById('login-error').innerText = 'Invalid email or password. Please try again.';
     }
 });
+
+// Function to show success message
+function showSuccessMessage(message) {
+    const successMessage = document.createElement('p');
+    successMessage.innerText = message;
+    successMessage.classList.add('success-message');
+    document.body.appendChild(successMessage);
+    setTimeout(function () {
+        successMessage.remove();
+    }, 3000);
+}
